883c6f847e6c383b5789a01e1a432e2e1d70f921
zhenya194 <zhenya.pro1753@gmail.com>
2026-02-25 13:12:04 +0200

    Update admin.py

--- handlers/admin.py
+++ handlers/admin.py
@@ -7,14 +7,12 @@ class Admin(commands.Cog):
 
     @commands.command(name="clear")
     @commands.has_role("admin")
-    async def purge(self, ctx, amount: int):
+    async def purge(self, ctx, amount:int):
         await ctx.channel.purge(limit=amount + 1)
-        await ctx.send(f"Deleted {amount} messages.", delete_after=5)
-
     @purge.error
     async def purge_error(self, ctx, error):
         if isinstance(error, commands.MissingRole):
-            await ctx.send(f"{ctx.author.mention}, you're not an admin.", delete_after=3)
+            await ctx.send(f"{ctx.author.mention}, you're not an admin.", delete_after=7)
 
 async def setup(bot):
     await bot.add_cog(Admin(bot))
